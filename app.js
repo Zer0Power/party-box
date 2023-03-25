bcaeaa07dce80ceb64b0814f328219cfc8c12bfd
Zer0Power <zeroopoower@gmail.com>
2023-03-25 14:54:25 +0330

    feat : message Event

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 //Packages
 
 const fs = require('fs');
-const dotenv = require("dotenv").config();
+const path = require('path');
+const dotenv = require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
 //---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
@@ -13,3 +14,25 @@ const adminsID = [1117515898]
 const bot = new TelegramBot(token, { polling: true });
 
 //---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
+// Message Event 
+
+bot.on('message', async (message) => {
+
+    // Ignore messages that are more than 2 minutes old
+    const currentTime = Math.floor(Date.now() / 1000);
+    const messageTime = message.date;
+    const ageInSeconds = currentTime - messageTime;
+    const maxAgeInSeconds = 2 * 60; // 2 minutes
+    if (ageInSeconds > maxAgeInSeconds) {
+        return;
+    }
+
+
+    // Check if the user is an admin or not
+    if (adminsID.includes(message.chat.id)) { 
+       
+    }
+    else {
+        bot.sendMessage(message.chat.id, '❌ You are not authorized to perform this action.');
+    }
+});
