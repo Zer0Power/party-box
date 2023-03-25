4e3c9dd44b7c3367fb672113e96e519bba93a281
Zer0Power <zeroopoower@gmail.com>
2023-03-25 15:06:55 +0330

    feat: download event

--- app.js
+++ app.js
@@ -29,10 +29,39 @@ bot.on('message', async (message) => {
 
 
     // Check if the user is an admin or not
-    if (adminsID.includes(message.chat.id)) { 
-       
+    if (adminsID.includes(message.chat.id)) {
+
+        if (message.audio) {
+
+            bot.sendMessage(message.chat.id, `📫 File ${message.audio.title} recieved , download started!`)
+
+            //Define Path and File Name
+            const fileId = message.audio.file_id;
+            const fileSize = message.audio.file_size;
+            const filePath = `/var/lib/mopidy/musics/telmusics/${message.audio.file_name}`;
+
+
+            if (fileSize > 50 * 1024 * 1024) { // check if file is too big
+                bot.sendMessage(message.chat.id, `❌ Sorry, the file is too big to be sent.`);
+            } else {
+                bot.downloadFile(fileId, '/var/lib/mopidy/musics/telmusics/').then(() => {
+                    const fileStream = fs.createWriteStream(filePath);
+                    bot.getFileStream(fileId).pipe(fileStream);
+                    fileStream.on('finish', () => {
+                        console.log(`File ${fileId} saved to ${filePath}`);
+                        bot.sendMessage(message.chat.id, `📥 File ${message.audio.title} saved to server!`);
+                    });
+                });
+            }
+        } else {
+
+            bot.sendMessage(message.chat.id, `I'm sorry, I don't understand what you're saying.`);
+
+        }
+
+
     }
     else {
-        bot.sendMessage(message.chat.id, '❌ You are not authorized to perform this action.');
+        bot.sendMessage(message.chat.id, `❌ You are not authorized to perform this action.\n Your ID : ${message.chat.id}`);
     }
 });
