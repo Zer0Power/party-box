47ccf10bcd0b3416469999c4e2246175530b14f9
Zer0Power <zeroopoower@gmail.com>
2023-03-25 14:28:10 +0330

    feat : define bot and adminsID

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ const dotenv = require("dotenv").config();
 const TelegramBot = require('node-telegram-bot-api');
 
 //---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
+//Define Bot And Admin  
+
+const token = process.env.token;
+const adminsID = [1117515898]
+const bot = new TelegramBot(token, { polling: true });
+
+//---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
