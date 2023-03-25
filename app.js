689db412656d91160750f69b8af375326e48c76d
Zer0Power <zeroopoower@gmail.com>
2023-03-25 14:24:11 +0330

    feat: add packages

---
+++ app.js
@@ -0,0 +1,8 @@
+//---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
+//Packages
+
+const fs = require('fs');
+const dotenv = require("dotenv").config();
+const TelegramBot = require('node-telegram-bot-api');
+
+//---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
